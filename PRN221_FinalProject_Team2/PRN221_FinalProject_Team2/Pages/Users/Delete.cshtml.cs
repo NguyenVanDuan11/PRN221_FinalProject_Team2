de20f41ca5de2f2bcc0cb4ee9b18a5b4f05a40a6
dungcthe150178 <105591953+dungcthe150178@users.noreply.github.com>
2023-04-21 15:03:19 +0700

    CrudUsers

---
+++ PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_FinalProject_Team2.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
