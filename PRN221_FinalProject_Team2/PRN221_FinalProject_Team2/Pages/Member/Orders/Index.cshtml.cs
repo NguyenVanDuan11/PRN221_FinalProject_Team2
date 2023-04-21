a16bdf45b92b2d29010aa0ee7242eed53652d6fe
Nguyen Van Duan <119272062+NguyenVanDuan11@users.noreply.github.com>
2023-04-21 12:42:00 +0700

    add index to product . orders in memember

---
+++ PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Pages/Member/Orders/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_FinalProject_Team2.Pages.Member.Orders
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
