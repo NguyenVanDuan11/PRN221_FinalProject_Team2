c99fe8c8852a1c96dd71606f06ae460475ee1fef
huanviptq <huanviptq46@gmail.com>
2023-04-21 21:03:40 +0700

    display list manage product and category

--- PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Pages/Admin/Categories/Index.cshtml.cs
+++ PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Pages/Admin/Categories/Index.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN221_FinalProject_Team2.Models;
 
 namespace PRN221_FinalProject_Team2.Pages.Admin.Categories
 {
+    [BindProperties]
     public class IndexModel : PageModel
     {
+        private readonly PRN221DBContext _db;
+
+        public IndexModel(PRN221DBContext db)
+        {
+            _db = db;
+        }
+
+        public List<Category> Categories { get; set; }
+
         public void OnGet()
         {
+            Categories = _db.Categories.ToList();
         }
     }
 }
--- PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Pages/Admin/Products/Index.cshtml.cs
+++ PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Pages/Admin/Products/Index.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PRN221_FinalProject_Team2.Models;
 
 namespace PRN221_FinalProject_Team2.Pages.Admin.Products
 {
+    [BindProperties]
     public class IndexModel : PageModel
     {
+        private readonly PRN221DBContext _db;
+
+        public IndexModel(PRN221DBContext db)
+        {
+            _db = db;
+        }
+
+        public List<Product> Products { get; set; }
+
         public void OnGet()
         {
+            Products = _db.Products.Include(c => c.Category).ToList();
         }
     }
 }
