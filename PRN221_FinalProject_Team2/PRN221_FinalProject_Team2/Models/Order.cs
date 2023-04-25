57fb77630a80211d9b52ef4621ccc5f46791e220
Nguyen Van Duan <119272062+NguyenVanDuan11@users.noreply.github.com>
2023-04-26 03:24:39 +0700

    update vlidate checkout

--- PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Models/Order.cs
+++ PRN221_FinalProject_Team2/PRN221_FinalProject_Team2/Models/Order.cs
@@ -15,16 +15,21 @@ namespace PRN221_FinalProject_Team2.Models
         public string? CustomerId { get; set; }
         public int? EmployeeId { get; set; }
         public DateTime? OrderDate { get; set; }
+        [Required(ErrorMessage ="Must input date")]
         public DateTime? RequiredDate { get; set; }
         public DateTime? ShippedDate { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:N}", ApplyFormatInEditMode = true)]
         public decimal? Freight { get; set; }
+        [Required(ErrorMessage = "Must input ")]
         public string? ShipName { get; set; }
+        [Required(ErrorMessage = "Must input ")]
         public string? ShipAddress { get; set; }
+        [Required(ErrorMessage = "Must input ")]
         public string? ShipCity { get; set; }
         public string? ShipRegion { get; set; }
         public string? ShipPostalCode { get; set; }
+        [Required(ErrorMessage = "Must input")]
         public string? ShipCountry { get; set; }
 
         public virtual Customer? Customer { get; set; }
